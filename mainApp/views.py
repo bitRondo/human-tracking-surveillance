f23744be53dc8ee5f93bfd0bbbf3f478d8e8caeb
deePro <disurawaru.17@cse.mrt.ac.lk>
2020-03-24 00:03:41 +0530

    index, login html files created and admin, urls, views files are changed
    Downgraded the Django version to 2.2.11. It remedied the error of logging in as an admin to the project.
    Added some data to the database, using shell. Now instead of plaintext, the index function of views renders index.html file

--- mainApp/views.py
+++ mainApp/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello! This is the index of mainApp!')
+    return render(request, 'mainApp/index.html')
+
+def login(request):
+    return render(request, 'mainApp/login.html')
