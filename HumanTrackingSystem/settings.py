8d8e6d6a8ade950d820c9929d20382e8ecc21815
deePro <disurawaru.17@cse.mrt.ac.lk>
2020-03-29 00:49:11 +0530

    Changed the Project structure
    Now, the components 'Account Management' and 'Statistics (Management)' are separate apps which contain models and views related to each. There is no 'mainApp'. Directories are added for Video Analysis and Static components.

--- HumanTrackingSystem/settings.py
+++ HumanTrackingSystem/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accountManagement.apps.AccountmanagementConfig',
+    'statisticsManagement.apps.StatisticsmanagementConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'mainApp.apps.MainappConfig'
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
--- mainApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return render(request, 'mainApp/index.html')
-
-def login(request):
-    return render(request, 'mainApp/login.html')
