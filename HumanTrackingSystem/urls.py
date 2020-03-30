4336b5ca3e70a5db5f7aa009882ef1fa647cdf96
deePro <disurawaru.17@cse.mrt.ac.lk>
2020-03-30 17:34:49 +0530

    Login and Logout complete

--- HumanTrackingSystem/urls.py
+++ HumanTrackingSystem/urls.py
@@ -19,5 +19,4 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accountManagement.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- accountManagement/urls.py
+++ accountManagement/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user = True), name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page = '/'), name = 'logout'),
 ]
