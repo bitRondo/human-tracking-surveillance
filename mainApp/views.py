093a3ce22eebaa8138f312c00d2d94d33fd461b1
Nuwanthika <nuwanthikaw.17@cse.mrt.ac.lk>
2020-04-06 22:39:55 +0530

    installed the mainApp

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Surveillance system to detect and track humans")
+
 
 # Create your views here.
