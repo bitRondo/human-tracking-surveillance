615be8a28cf78c487608cd65fd2ac89b25f06ac4
Nuwanthika <nuwanthikaw.17@cse.mrt.ac.lk>
2020-04-06 22:39:55 +0530

    installed the mainApp

--- mainApp/models.py
+++ mainApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
+class Recipient(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+
 # Create your models here.
