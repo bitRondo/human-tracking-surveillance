3fbd580a77c23fe90d5767b0ea0c3072e1aa2bab
Nuwanthika <nuwanthikaw.17@cse.mrt.ac.lk>
2020-04-06 23:14:34 +0530

    create a form for add recipient for security alert

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Recipient
+from .forms import RecipientForm
 
 
 def index(request):
     return HttpResponse("Surveillance system to detect and track humans")
 
+
+def recipient(request):
+    form = RecipientForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context = {
+        'form': form
+    }
+    return render(request,'recipient.html',context)
 
 # Create your views here.
