69f3d23cd2820cdd3d8d1cbf499e23b22ae79404
Nuwanthika <nuwanthikaw.17@cse.mrt.ac.lk>
2020-04-11 06:13:16 +0530

    Do changes

--- HumanTrackingSystem/settings.py
+++ HumanTrackingSystem/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'mainApp.apps.MainappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mainApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Recipient
-
-admin.site.register(Recipient)
-# Register your models here.
