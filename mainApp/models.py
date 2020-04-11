728dcdc749ab9b3e821f2dd9eaf9ad84b231f46b
Nuwanthika <nuwanthikaw.17@cse.mrt.ac.lk>
2020-04-11 06:13:16 +0530

    Do changes

--- mainApp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Recipient(models.Model):
-    name = models.CharField(max_length=200)
-    email = models.CharField(max_length=200)
-
-# Create your models here.
