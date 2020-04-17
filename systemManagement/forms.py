01077491aef59f160984b9b9e294dae1d517c2e1
Disura <disurayw@gmail.com>
2020-04-17 20:48:13 +0530

    Styling - Halfway Done

--- systemManagement/forms.py
+++ systemManagement/forms.py
@@ -11,29 +11,43 @@ class SystemSettingsForm(forms.Form):
 
     auto_switching = forms.ChoiceField(
         choices = auto_switching_choices,
-        widget = forms.RadioSelect(attrs = {'oninput':'toggleAutoSwitching()'}),
+        widget = forms.RadioSelect(
+            attrs = {
+                'oninput':'toggleAutoSwitching()', 
+                'class':'form-check-input',
+                'style' : 'margin-left: 20px'
+            }
+        ),
     )
 
     business_start = forms.TimeField(
-        widget = TimeInput,
+        widget = TimeInput(
+            attrs = {'class' : 'form-control col-sm-3', 'style' : 'margin-right: 10px; margin-left: 5px;'}
+        ),
         label = 'Start: ',
         required = False,
     )
 
     business_end = forms.TimeField(
-        widget = TimeInput,
+        widget = TimeInput(
+            attrs = {'class' : 'form-control col-sm-3', 'style' : 'margin-right: 10px; margin-left: 5px;'}
+        ),
         label = 'Stop: ',
         required = False,
     )
 
     security_start = forms.TimeField(
-        widget = TimeInput,
+        widget = TimeInput(
+            attrs = {'class' : 'form-control col-sm-3', 'style' : 'margin-right: 10px; margin-left: 5px;'}
+        ),
         label = 'Start: ',
         required = False,
     )
 
     security_end = forms.TimeField(
-        widget = TimeInput,
+        widget = TimeInput(
+            attrs = {'class' : 'form-control col-sm-3', 'style' : 'margin-right: 10px; margin-left: 5px;'}
+        ),
         label = 'Stop: ',
         required = False,
     )
